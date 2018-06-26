38f5ea6b649e55b2a80e4080f785c551b4db9a7e
jgsimpao <joel.simpao@outlook.com>
2018-06-27 02:40:54 +0800

    Modified index.js and package.json

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const fs = require('fs');
 
+const PORT = process.env.PORT || 5000;
+
 fs.readFile('web/index.html', (err, html) => {
     if (err) {
         throw err;
@@ -8,8 +10,12 @@ fs.readFile('web/index.html', (err, html) => {
 
     const server = http.createServer((req, res) => {
         res.statusCode = 200;
-        res.setHeader('Content-type', 'text/html');
+        res.setHeader('Content-Type', 'text/html');
         res.write(html);
         res.end();
+    });
+
+    server.listen(PORT, () => {
+        
     });
 });
