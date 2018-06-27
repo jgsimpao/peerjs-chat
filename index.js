c191d5dc5300c1b58c46777f031d2f578adc2d2d
jgsimpao <joel.simpao@outlook.com>
2018-06-27 13:38:43 +0800

    Incorporated Express framework to project

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
 const http = require('http');
-const fs = require('fs');
+//const fs = require('fs');
+//const path = require('path');
+const express = require('express');
 
-const PORT = process.env.PORT || 5000;
+const app = express();
+//const publicPath = path.resolve(__dirname, 'public/css');
 
-fs.readFile('web/index.html', (err, html) => {
-    if (err) {
-        throw err;
+//app.use('/css', express.static('public/css'));
+/*
+app.get('/', (req, res) => {
+    res.render('public/index');
+});
+*/
+
+/*
+app.listen(port, () => {
+    console.log("http://localhost:${port}");
+});
+*/
+
+app.use(express.static(__dirname + '/public'));
+const port = process.env.PORT || 5000;
+http.createServer(app).listen(port);
+/*
+const server = http.createServer((req, res) => {
+    //if (req.url.indexOf('.html') !== -1) {
+        fs.readFile(__dirname + '/public/index.html', (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        });
+        console.log(req.url.indexOf('.html'));
+    //}
+    
+    if (req.url.indexOf('.css') !== -1) {
+        fs.readFile(__dirname + '/public/css/style.css', (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.write(data);
+            res.end();
+        });
     }
 
-    const server = http.createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.write(html);
-        res.end();
-    });
-
-    server.listen(PORT, () => {
-        
-    });
+    if (req.url.indexOf('.js') !== -1) {
+        fs.readFile(__dirname + '/public/js/script.js', (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/javascript'});
+            res.write(data);
+            res.end();
+        });
+    }
+    
 });
+
+server.listen(port);
+*/
