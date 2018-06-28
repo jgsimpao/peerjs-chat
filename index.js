2eb2530ec7b789660e75ad7e077191bec8567f23
jgsimpao <joel.simpao@outlook.com>
2018-06-28 11:53:13 +0800

    Modified index.js

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
-const ExpressPeerServer = require('peer').ExpressPeerServer;
+//const ExpressPeerServer = require('peer').ExpressPeerServer;
 
 app.use(express.static(__dirname + '/public'));
 
-const port = process.env.PORT || 9000;
-//const server = app.listen(port);
-const server = app.listen(5000);
-const options = {debug: true};
-const peerserver = ExpressPeerServer(server, options);
+const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
+const ip = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '0.0.0.0';
 
-app.use('/api', peerserver);
+//const server = http.createServer(myServerFunction);
+//server.listen(port, ip);
+
+app.listen(port, ip);
+
+//const port = process.env.PORT || 9000;
+//const server = app.listen(port);
+//const server = app.listen(5000);
+//const options = {debug: true};
+//const peerserver = ExpressPeerServer(server, options);
+
+//app.use('/api', peerserver);
