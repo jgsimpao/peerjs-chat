2387655fabdff9d461a20682a03a196f70a70190
jgsimpao <joel.simpao@outlook.com>
2018-06-28 15:02:35 +0800

    Implemented multi-user connections, and code cleanup of script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,52 +1,33 @@
 $(document).ready(function() {
     const peer = new Peer();
-    var conns = [];
+    var peerIDs = [];
     
-    peer.on('open', function(id) {
-        $('#user-id').html(id);
+    peer.on('open', function(userID) {
+        $('#user-id').html(userID);
     });
     
     $('#new-peer-btn').click(function() {
-        //$('#peer-list').append('<h6 class="peer-id">' + $('#new-peer-id').val() + '</h6>');
-        $('#peer-list').html($('#new-peer-id').val());
-        /*
-        $('.peer-id').each(function() {
-            //const conn = peer.connect('dest-peer-id');
-            conns.push(peer.connect($(this).val()));
-            alert('conns.push: ' + $(this).val());
-        });
-        */
-       /*
-        conn = peer.connect($('#new-peer-id').val());
-        alert('peer.connect: ' + $('#new-peer-id').val());
-        */
+        $('#peer-list').append('<p class="peer-id">' + $('#new-peer-id').val() + '</p>');
+        peerIDs.push($('#new-peer-id').val());
     });
     
     // Send messages
     $('#new-message-btn').click(function() {
         $('#chat-window').append('<p class="chat-message">' + $('#new-message-text').val() + '</p>');
-        /*
-        conns.forEach(function(conn) {
+        
+        peerIDs.forEach(function(peerID) {
+            var conn = peer.connect(peerID);
+
             conn.on('open', function() {
-                //conn.send('Hello!');
                 conn.send($('#new-message-text').val());
-                alert('conn.send: ' + $('#new-message-text').val());
             });
-        });
-        */
-        const conn = peer.connect($('#peer-list').html());
-        conn.on('open', function() {
-            conn.send($('#new-message-text').val());
-            alert('conn.send: ' + $('#new-message-text').val());
         });
     });
 
     // Receive messages
     peer.on('connection', function(conn) {
         conn.on('data', function(data) {
-            //console.log(data);
             $('#chat-window').append('<p class="chat-message">' + data + '</p>');
-            alert('$("#chat-window").append: ' + data);
         });
     });
 });
